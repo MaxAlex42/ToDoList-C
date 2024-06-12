f69ce16b6092cb9c236eb85b09a5f86bbe0adc14
Philipp Michael Arbeitstein <philippar@edu.aau.at>
2024-06-12 17:43:03 +0200

    feat: Added Basic Scrollable ListView UI and functionallity

--- todolist.c
+++ todolist.c
@@ -1,33 +1,115 @@
 #include <gtk/gtk.h>
 
-void on_main_window_destroy() {
-    gtk_main_quit();
+/*
+    Enum to define the columns in the GtkListStore (ListView)
+    and holds the number of total columns
+*/
+enum
+{
+    COLUMN_DONE,
+    COLUMN_TASK,
+    NUM_COLUMNS
+};
+
+// Callback function for the "destroy" signal of the main window
+void on_main_window_destroy()
+{
+    gtk_main_quit(); // Quits the app
 }
 
-int main(int argc, char *argv[]) {
-    GtkBuilder *builder;
-    GtkWidget *window;
+// Function to add a new todo item to the GtkListStore
+void add_todo_item(GtkListStore *list, gboolean done, const gchar *task)
+{
+    GtkTreeIter iter;                     // Iterator to access the row in the List
+    gtk_list_store_append(list, &iter);   // Adds a new row to the GtkListStore and sets iter to that row
+    gtk_list_store_set(list, &iter,       // Set Values in the row where iter points to
+                       COLUMN_DONE, done, // Sets the "done" status
+                       COLUMN_TASK, task, // Sets the task description
+                       -1);               // Marks the end of the variable arguments list
+}
 
-    gtk_init(&argc, &argv);
+/*
+    Callback function for the "toggled" signal of the GtkCellRendererToggle.
+    In easier words ... this is the function that gets executed, if someone clicks a
+    checkbox
+*/
+void on_toggled(GtkCellRendererToggle *renderer, gchar *path, gpointer user_data)
+{
+    GtkListStore *store = GTK_LIST_STORE(user_data);
+    GtkTreeIter iter;
+    gboolean active;
 
-    builder = gtk_builder_new();
-    if (gtk_builder_add_from_file(builder, "todo.glade", NULL) == 0) {
-        g_printerr("Error loading file: interface.glade\n");
+    // Gets the GtkTreeIter corresponding to the given path
+    gtk_tree_model_get_iter_from_string(GTK_TREE_MODEL(store), &iter, path);
+    // Gets the current value of the "done" column
+    gtk_tree_model_get(GTK_TREE_MODEL(store), &iter, COLUMN_DONE, &active, -1);
+
+    // Toggles the "done" status
+    active = !active;
+    // Updates the "done" status in the GtkListStore
+    gtk_list_store_set(store, &iter, COLUMN_DONE, active, -1);
+}
+
+// Main function
+int main(int argc, char *argv[])
+{
+    GtkBuilder *builder;       // Builder object to load the UI from the Glade file
+    GtkWidget *window;         // Main application window
+    GtkTreeView *tree_view;    // TreeView to display the todo list
+    GtkListStore *list_store;  // ListStore to hold the todos
+    GtkTreeViewColumn *column; // Variable for the columns in the TreeView
+    GtkCellRenderer *renderer; // Variable for the cell renderers
+
+    gtk_init(&argc, &argv); // Initializes the GTK library
+
+    builder = gtk_builder_new(); // Creates a new GtkBuilder object
+    // Loads the UI definition from the Glade file
+    if (gtk_builder_add_from_file(builder, "todo.glade", NULL) == 0)
+    {
+        g_printerr("Error loading file: todo.glade\n");
         return 1;
     }
 
-    window = GTK_WIDGET(gtk_builder_get_object(builder, "application"));
-    if (!window) {
-        g_printerr("Unable to find object with id 'main_window'\n");
+    window = GTK_WIDGET(gtk_builder_get_object(builder, "application")); // Gets the main window object from the builder
+    if (!window)
+    {
+        g_printerr("Unable to find object with id 'application'\n");
         return 1;
     }
 
-    gtk_builder_connect_signals(builder, NULL);
+    gtk_window_set_default_size(GTK_WINDOW(window), 800, 600); // Sets the default size of the window to 800x600
 
-    gtk_widget_show_all(window);
+    gtk_builder_connect_signals(builder, NULL); // Connects the signal handlers defined in the Glade file
 
-    gtk_main();
+    tree_view = GTK_TREE_VIEW(gtk_builder_get_object(builder, "items_view")); // Gets the TreeView object from the builder
 
-    return 0;
+    // Creates a new GtkListStore with two columns: one for booleans and one for strings
+    list_store = gtk_list_store_new(NUM_COLUMNS, G_TYPE_BOOLEAN, G_TYPE_STRING);
+
+    /*
+        Sets the model for the TreeView meaning that the TreeView will use the list_store as it's "data model"
+    */
+    gtk_tree_view_set_model(tree_view, GTK_TREE_MODEL(list_store));
+    // g_object_unref(list_store);                                     // Decreases the reference count of the list store (really nessecary?)
+
+    // Add the checkbox column
+    renderer = gtk_cell_renderer_toggle_new();                                                        // Creates a new toggle cell renderer
+    g_signal_connect(renderer, "toggled", G_CALLBACK(on_toggled), list_store);                        // Connects the "toggled" signal to the callback function
+    column = gtk_tree_view_column_new_with_attributes("Done", renderer, "active", COLUMN_DONE, NULL); // Creates a new column with the toggle renderer
+    gtk_tree_view_append_column(tree_view, column);                                                   // Adds the column to the TreeView
+
+    // Add the task column
+    renderer = gtk_cell_renderer_text_new();                                                        // Creates a new text cell renderer
+    column = gtk_tree_view_column_new_with_attributes("Task", renderer, "text", COLUMN_TASK, NULL); // Creates a new column with the text renderer
+    gtk_tree_view_append_column(tree_view, column);                                                 // Adds the column to the TreeView
+
+    // Add sample todos
+    add_todo_item(list_store, FALSE, "Philipp");
+    add_todo_item(list_store, FALSE, "Max");
+
+    gtk_widget_show_all(window); // Shows the main window and all its child widgets
+
+    gtk_main(); // Enters the GTK main loop
+
+    return 0; // Exits the program
 }
-// gcc `pkg-config --cflags gtk+-3.0` -o ToDoList test.c `pkg-config --libs gtk+-3.0` -rdynamic
